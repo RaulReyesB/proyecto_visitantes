c2f43732c4d7227f2ebd63ca98ec5b3f16623834
Raul <raulreyesbatalla@outlook.es>
2024-05-09 09:46:39 -0600

    agregando detalles para poder hacer arrancar el servidor

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import router from './routes/index.routes';
+import router from './routes/index.routes.js';
+import { config } from "dotenv";
+
+//setings
+dotenv.config({ path: ".env" });
 
 dotenv.config({path: "src/.env"})
 const app=express();
 
 app.set('view engine', 'pug')
 app.set('views', 'src/views')
+
+app.listen(process.env.PORT, () => {
+  console.log("Server on port: " + process.env.PORT);
+});
+
 
 app.use("/", router);

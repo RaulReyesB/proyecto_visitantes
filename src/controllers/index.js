580fe25ae1aef34e3537bef650eac6d33b42461e
Raul <raulreyesbatalla@outlook.es>
2024-05-09 15:56:15 -0600

    Agregando ultimas funciones para el historial de visitas

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,19 +1,5 @@
 import Visit from "../models/visit.js";
 
-const registros = [
-  {
-    nombre: "jacinta",
-    telefono: "1234567890",
-    correo: "jacinta@gmail.com",
-    curp: "123456789012345678",
-    identificacion: "INE",
-    departamento: "Comunicacion",
-    procedenciaa: "Hidalgo",
-    ninos: "0",
-    gafete: "772",
-  },
-];
-
 const index = (req, res) => {
   res.render("index", {
     nombrePagina: "Inicio",
@@ -27,33 +13,39 @@ const register = (req, res) => {
   });
 };
 
-const history = (req, res) => {
-  res.render("history", {
-    nombrePagina: "Historial de visitas",
-    descripcion: "Historial de visitantes de Radio y Television Hidalgo",
-    registros: registros,
-  });
+const history = async (req, res) => {
+  try {
+    const registros = await Visit.findAll(); // Recupera todos los registros de visitas de la base de datos
+    res.render("history", {
+      nombrePagina: "Historial de visitas",
+      descripcion: "Historial de visitantes de Radio y Television Hidalgo",
+      registros: registros, // Pasa los registros a la vista
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error interno del servidor");
+  }
 };
 
 const insertVisit = async (req, res) => {
   try {
-      // Crea un nuevo registro utilizando los datos del formulario
-      const newVisit = await Visit.create({
-          name: req.body.name,
-          phone: req.body.phone,
-          email: req.body.email,
-          CURP: req.body.CURP,
-          identification: req.body.identification,
-          department: req.body.department,
-          origin: req.body.origin,
-          children: req.body.children,
-          badge: req.body.badge // Corregido el nombre del campo
-        });
-        res.send('Registro exitoso');
+    // Crea un nuevo registro utilizando los datos del formulario
+    const newVisit = await Visit.create({
+      name: req.body.name,
+      phone: req.body.phone,
+      email: req.body.email,
+      CURP: req.body.CURP,
+      identification: req.body.identification,
+      department: req.body.department,
+      origin: req.body.origin,
+      children: req.body.children,
+      badge: req.body.badge, // Corregido el nombre del campo
+    });
+    res.send("Registro exitoso");
   } catch (error) {
-      console.error(error);
-      res.status(500).send('Error interno del servidor');
+    console.error(error);
+    res.status(500).send("Error interno del servidor");
   }
 };
 
-export { history, index, register, insertVisit};
+export { history, index, register, insertVisit };

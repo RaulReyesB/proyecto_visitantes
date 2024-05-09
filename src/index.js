dd5edc589c77e6d876abfa66e1af36d4c6177377
Raul <raulreyesbatalla@outlook.es>
2024-05-09 09:57:44 -0600

    creando documentacion de el index.js de inicio

--- src/index.js
+++ src/index.js
@@ -3,18 +3,25 @@ import express from 'express';
 import router from './routes/index.routes.js';
 import { config } from "dotenv";
 
+//inicializando express y dandole los atributos a app
+const app=express();
+
 //setings
 dotenv.config({ path: ".env" });
 
+//habilitando env para poder ocupar variables de entorno 
 dotenv.config({path: "src/.env"})
-const app=express();
+
+//Definimos la carpeta para los recursos Public
+app.use(express.static("./src/public"));
 
 app.set('view engine', 'pug')
 app.set('views', 'src/views')
 
+//agregando el puerto para poder levantar el servidor 
 app.listen(process.env.PORT, () => {
   console.log("Server on port: " + process.env.PORT);
 });
 
-
+//creadno ruta madre para las rutas
 app.use("/", router);

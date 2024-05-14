b3a7bcd2ba4c0ae599cba821c1289dc0fc6de6dc
Ederdal <degarcruz2004@gmail.com>
2024-05-14 14:52:57 -0600

    Parseo de fechas y horas en la bd

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -31,15 +31,21 @@ const interns = (req, res) => {
 // Función asincrónica para obtener y renderizar el historial de visitas
 const history = async (req, res) => {
   try {
-    // todos los registros de la tabla
+    // Todos los registros de la tabla
     const allRegistros = await Visit.findAll();
 
     // Filtrar los registros donde la salida no es nula
     const registros = allRegistros.filter((registro) => registro.exit !== null);
 
+    // Parsear las fechas antes de pasarlas a la plantilla
+    registros.forEach(registro => {
+      registro.createdAt = formatDate(registro.createdAt);
+      // Si es necesario, también puedes parsear la fecha de salida aquí
+    });
+
     res.render("history", {
       nombrePagina: "Historial de visitas",
-      descripcion: "Historial de visitantes de Radio y Television Hidalgo",
+      descripcion: "Historial de visitantes de Radio y Televisión Hidalgo",
       registros: registros,
     });
   } catch (error) {
@@ -47,6 +53,20 @@ const history = async (req, res) => {
     res.status(500).send("Error interno del servidor");
   }
 };
+
+// Función para formatear la fecha
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  
+  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+}
+
 
 // Función para renderizar la página de registro de usuarios
 const renderRegisterPage = (req, res) => {
@@ -120,6 +140,14 @@ const insertIntern = async (req, res) => {
   }
 };
 
+
+
+
+
+
+
+
+
 // Función asincrónica para autenticar al usuario al iniciar sesión
 const authenticateUser = async (req, res) => {
   try {
@@ -163,6 +191,15 @@ const authenticateUser = async (req, res) => {
       });
     }
 
+    // Verificar si el usuario está inactivo
+    if (user.status === false ) {
+      console.log(user.status)
+      return res.render("login", {
+        namePage: "Iniciar Sesión en Radio y Television Hidalgo",
+        errors: [{ msg: "Tu cuenta está inactiva. Por favor, ponte en contacto con el administrador." }],
+      });
+    }
+    // Si el usuario está activo, continuar con el proceso de inicio de sesión
     req.session.user = user;
 
     res.render("index", {
@@ -175,6 +212,11 @@ const authenticateUser = async (req, res) => {
     res.status(500).send("Error interno del servidor");
   }
 };
+
+
+
+
+
 
 // Función para renderizar la página de inicio de sesión
 const login = (req, res) => {

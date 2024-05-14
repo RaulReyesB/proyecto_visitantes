65f7d0879a282aefd36c92be017148459afca777
Raul <raulreyesbatalla@outlook.es>
2024-05-14 10:38:24 -0600

    agregando restriccion de tipos de usuarios

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,9 +1,29 @@
 const requireAuth = (req, res, next) => {
-    if (req.session && req.session.user) {
-      next();
-    } else {
-      res.redirect('/iniciarSesion');
-    }
-  };
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.redirect("/iniciarSesion");
+  }
+};
 
-  export default requireAuth;
+// Middleware para verificar si el usuario es un superUsuario o administrador
+const requireSuperUser = (req, res, next) => {
+  if (
+    req.session.user &&
+    (req.session.user.type === "superUsuario" ||
+      req.session.user.type === "administrador")
+  ) {
+    next();
+  } else {
+    const userName = req.session.user ? req.session.user.name : "Desconocido";
+    const userType = req.session.user ? req.session.user.type : "Desconocido";
+
+    console.log(
+      `Intento de acceso no autorizado a la página de historial por parte del usuario: ${userName} y es de tipo: ${userType}`
+    );
+
+    res.redirect("/iniciarSesion");
+  }
+};
+
+export { requireSuperUser, requireAuth };
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,26 +1,30 @@
 import { Router } from "express";
-import express from 'express';
+import express from "express";
 
-import { index, register, history, insertVisit, login, authenticateUser } from "../controllers/index.js";
-import requireAuth from "../middlewares/auth.js";
+import {
+  index,
+  register,
+  history,
+  insertVisit,
+  login,
+  authenticateUser,
+} from "../controllers/index.js";
+import { requireAuth, requireSuperUser } from "../middlewares/auth.js";
 
 const router = Router();
 
 // Middleware para parsear datos codificados en URL
 router.use(express.urlencoded({ extended: true }));
 
-
 // Rutas protegidas que requieren autenticación (requireAuth)
 router.get("/", requireAuth, index);
 router.get("/registroVisitas", requireAuth, register);
 router.post("/registroVisitas", requireAuth, insertVisit);
-router.get("/historial", requireAuth, history);
+
+router.get("/historial", requireSuperUser, history); 
 
 // Rutas públicas (sin autenticación)
 router.get("/iniciarSesion", login);
 router.post("/iniciarSesion", authenticateUser);
 
-
 export default router;
-
-

d0a134890ccc9c3681b26a4b4c9ff2b524105e8d
angelR003 <220432@utxicotepec.edu.mx>
2024-05-16 10:17:30 -0600

    CAMPOS DE LA BD PARA NUEVOS REGISTROS DE PASANTES

--- src/models/Intern.js
+++ src/models/Intern.js
@@ -40,6 +40,58 @@ const Intern = db.define("tb_interns", {
     allowNull: false,
     defaultValue: 0,
   },
+  fileNumber: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  school: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  Mat: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  career: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  asignementDirec: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  adviser: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  numberHours: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  shedule: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  hoursxDay: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  period: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  totHours: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  Program: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  Observations: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
   entrance: {
     type: DataTypes.DATE,
     allowNull: false,

f35ba0a3bcbd58ad8bf56bb96c3c0da6ab11e6d6
Raul <raulreyesbatalla@outlook.es>
2024-05-16 11:11:08 -0600

    agrregando variables para mostrar la tabla de control de pasantes

--- src/controllers/interns.js
+++ src/controllers/interns.js
@@ -1,10 +1,11 @@
 import Intern from "../models/intern.js";
 
 const controllInterns = async(req, res) => {
-  const intert = await Intern.findAll()
+  const internt = await Intern.findAll()
   res.render("controllInterns", {
     namePage: "Control de Pasantes",
     description: "Bienvenido a el control de Pasantes",
+    internt :internt 
   });
 };
 

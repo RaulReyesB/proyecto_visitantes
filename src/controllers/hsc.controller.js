1012bd59d72ace9e1361a70fecb02bebb9f59f8f
Ederdal <degarcruz2004@gmail.com>
2024-05-28 09:59:24 -0600

    Agregando el modelo de las tablas a la base de datos y la creacion automatica de estas, asi mismo las llaves foraneas estan echas

--- src/controllers/hsc.controller.js
+++ src/controllers/hsc.controller.js
@@ -2,23 +2,39 @@ import Intern from "../models/Intern.js";
 
 const historySC = async (req, res) => {
     try {
+        // Obtener todos los registros donde el servicio esté completado
+        const allRegistros = await Intern.findAll({
+            where: {
+                serviceCompleted: true,
+            },
+        });
 
-      const allRegistros = await Intern.findAll({
-        where: {
-          serviceCompleted: true,
-        },
-      });
-      const user = req.session.user
-      res.render("newvista", {
-        namePage: "Servicios Completos",
-        descripcion: "Historial de antiguos pasantes de Radio y Television Hidalgo",
-        registros: allRegistros,
-        user: user,
-      });
+        // Actualizar el campo fileNumber y registrar el mes y el año
+        for (const registro of allRegistros) {
+            // Obtener el mes y el año actual
+            const currentDate = new Date();
+            const currentMonth = currentDate.getMonth() + 1; // Sumar 1 porque los meses se indexan desde 0
+            const currentYear = currentDate.getFullYear();
+
+            // Actualizar el campo fileNumber con el mes y el año actual
+            const updatedRegistro = await registro.update({
+                fileNumber: `${registro.fileNumber}-${currentMonth}-${currentYear}`,
+            });
+
+            console.log(`Registro actualizado: ${updatedRegistro.id}`);
+        }
+
+        const user = req.session.user;
+        res.render("newvista", {
+            namePage: "Servicios Completos",
+            descripcion: "Historial de antiguos pasantes de Radio y Television Hidalgo",
+            registros: allRegistros,
+            user: user,
+        });
     } catch (error) {
-      console.error("Error al obtener los registros:", error);
-      res.status(500).send("Error interno del servidor");
+        console.error("Error al obtener los registros:", error);
+        res.status(500).send("Error interno del servidor");
     }
-  };
-  
-export {historySC}
+};
+
+export { historySC };
